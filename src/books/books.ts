0badfb1b582f9a8fd49972ea9f00edfcf524d5d2
Anders Styve <anders.styve@gmail.com>
2024-01-26 14:08:56 +0100

    updated book descs and isbn

--- src/books/books.ts
+++ src/books/books.ts
@@ -1,20 +1,20 @@
 export const books = [
   {
     id: 1,
-    isbn: "12345",
+    isbn: "9781857231588",
     title: "Rendezvous with Rama",
     author: "Arthur C. Clarke",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, sequi dolores autem unde non suscipit eos eligendi minima maxime. Aliquid placeat facilis quo, aspernatur libero nisi sit facere! Totam rem vel, delectus dignissimos deleniti natus minima asperiores tempora. Repudiandae, repellat accusantium fugiat velit beatae exercitationem veritatis perspiciatis labore placeat pariatur?",
+      "Rendezvous with Rama by Arthur C. Clarke is a captivating tale set in the 22nd century, where a massive cylindrical object, named Rama, enters our solar system. A team of explorers embarks on a thrilling mission to uncover the mysteries of this enigmatic extraterrestrial artifact, leading to profound discoveries and unforeseen challenges.",
     cover_img_url: "https://m.media-amazon.com/images/I/91EQ0zyctlL._AC_UF1000,1000_QL80_.jpg",
   },
   {
     id: 2,
-    isbn: "998877",
+    isbn: "9780142437247",
     title: "Moby Dick",
     author: "Herman Melville",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, sequi dolores autem unde non suscipit eos eligendi minima maxime. Aliquid placeat facilis quo, aspernatur libero nisi sit facere! Totam rem vel, delectus dignissimos deleniti natus minima asperiores tempora. Repudiandae, repellat accusantium fugiat velit beatae exercitationem veritatis perspiciatis labore placeat pariatur?",
+      "Moby Dick by Herman Melville unfolds as Ishmael, a sailor, joins the obsessive Captain Ahab on the whaling ship Pequod. The journey transforms into a relentless pursuit of the elusive white whale, Moby Dick, exploring themes of revenge, obsession, and the profound mysteries of the sea in this timeless classic.",
     cover_img_url: "https://m.media-amazon.com/images/I/81R91ODA9DL._AC_UF1000,1000_QL80_.jpg",
   },
 ];
